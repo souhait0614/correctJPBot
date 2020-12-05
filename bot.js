cb3da7f75523e1006b39e77e45be776469d7fe8f
hakunagi <hakunagi.rei@gmail.com>
2020-12-05 23:37:05 +0000

    バージョン2完成

--- bot.js
+++ bot.js
@@ -8,20 +8,58 @@ client.on('ready', () => {
   console.log(`${client.user.tag}にログインしました`);
 });
 
-client.on('message', msg => {
-  if (msg.author.bot || !msg.member.roles.cache.find(r => r.name === "怪レい者")) return;
-  request.get({
-    uri: URL,
-    headers: {
-      'Content-type': 'application/json'
-    },
-    qs: {
-      data: msg.content
-    },
-    json: true
-  }, function (err, req, data) {
-    msg.channel.send(data.data);
+function translate(text) {
+  return new Promise((data) => {
+    request.get({
+      uri: URL,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      qs: {
+        data: text
+      },
+      json: true
+    }, function (err, res, body) {
+      data(body.data);
+    });
   });
-});
+}
+
+client.on('message', async msg => {
+  if (msg.author.bot) return
+  if (msg.attachments.size > 0) return
+  for (const [, role] of (await msg.guild.roles.fetch()).cache) {
+    if (role.name === '怪レい日本语') {
+      for (const [id, ] of (await role.members)) {
+        if (msg.author.id === id) {
+
+          const sendText = await translate(msg.content);
+          if (!sendText || sendText === msg.content) return
+
+          const username = (msg.member.nickname || msg.author.username) + ' (怪レい日本语)'
+          const avatarURL = msg.author.avatarURL()
+          const webhook = await (async () => {
+            for (const [, webhook] of (await msg.channel.fetchWebhooks())) {
+              if (webhook.name === '怪レい日本语') return webhook
+            }
+          })() || await msg.channel.createWebhook('怪レい日本语')
+          await webhook.send(sendText, {
+            username,
+            avatarURL
+          })
+          await msg.delete()
+
+          // Submarin
+          if (msg.guild.id === '702430385916608592') {
+            await client.channels.cache.get('747087748535681074')
+              .send(msg.content)
+          }
+
+          return
+        }
+      }
+    }
+  }
+})
 
 client.login();
