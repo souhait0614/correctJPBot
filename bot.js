f840651b053394fdafdb1edbdb66ab14b229299c
hakunagi <hakunagi.rei@gmail.com>
2020-12-05 18:51:02 +0000

    バージョン1完成

--- bot.js
+++ bot.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const request = require('request');
+var URL = 'https://cjp.lil1.me/api';
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`${client.user.tag}にログインしました`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+  request.get({
+    uri: URL,
+    headers: {
+      'Content-type': 'application/json'
+    },
+    qs: {
+      data: msg
+    },
+    json: true
+  }, function (err, req, data) {
+    msg.channel.send(data.data);
+  });
 });
 
 client.login();
